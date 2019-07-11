197200f022af810ffa13d20776f4e80b3a515e07
mohamed <mohamed0group@gmail.com>
2019-07-11 16:50:25 +0200

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb+srv://mohamed0abdeen:<password>@cluster0-lxkco.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI:'mongodb+srv://root:root@mern-1-fre3u.mongodb.net/altras',
     secretOrKey: 'secret',
     cookieKey: ['key1', 'key2']
 };
